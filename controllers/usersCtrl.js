630c34927d7391bbf20e171b8097cd8b9c0e1391
snaike71 <kevin.lapert617@gmail.com>
2023-12-11 10:39:51 +0100

    first commit

---
+++ controllers/usersCtrl.js
@@ -0,0 +1,12 @@
+
+
+module.exports = {
+    getUsers: async function (req,res,next){
+        try {
+            
+        } catch (error) {
+            
+        }
+        res.send('respond with a resource');
+    }
+}
